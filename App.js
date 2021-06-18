9a09bea8f8dfe1949484f22637a732d68e768323
KevJSDevelopment <Kshhn12@gmail.com>
2021-06-17 20:49:03 -0400

    configuration for launch.json

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import TestingComponent from './TestingComponent';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>does this work?</Text>
       <StatusBar style="light" />
       <TestingComponent />
     </View>
